12bb5f0f2bcb67ad77a6f9c35c21940c7793ac96
TaysWeb <snelltararobin@gmail.com>
2023-05-18 16:41:01 +0200

    functions declared

---
+++ code .js
@@ -0,0 +1,18 @@
+
+
+
+
+
+function calculateGrade() {
+    const marks = [39 , 42, 65, 71, 84, 93] ;
+
+    // declaring my counter variable and additional variable 
+    let i = 0;
+    let total = marks.length([i]);
+    if ( i>= )
+   
+}
+
+function calculateAverage() {
+    
+}
