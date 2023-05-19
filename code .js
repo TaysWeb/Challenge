b5ebdb1ed06c4574cd355b09ba48ac8ea2baa317
TaysWeb <snelltararobin@gmail.com>
2023-05-19 04:56:11 +0100

    functions filled

--- code .js
+++ code .js
@@ -1,18 +1,31 @@
 
-
-
-
-
 function calculateGrade() {
     const marks = [39 , 42, 65, 71, 84, 93] ;
 
     // declaring my counter variable and additional variable 
     let i = 0;
     let total = marks.length([i]);
-    if ( i>= )
-   
+
+    // using the if statement by displaying the symbols 
+
+    if ( marks[0] <= 60 , i++ ) {           //value
+    console.log("Grade is F ")  ; 
+    }
+    if ( marks[1] <= 70 , i++ ) {              //value
+    console.log("Grade is D ")  ; 
+    }
+   if ( marks[2] <= 80 , i++ ) {               //value
+    console.log("Grade is  C ")  ; 
+   }
+   if ( marks[3] <= 90 , i++ ) {              //value
+    console.log("Grade is B ")  ; 
+   }
+   if ( marks[4] <= 100, i++ ) {              //value
+    console.log("Grade is A ")  ; 
+}
 }
 
 function calculateAverage() {
-    
+   let (i += total([i])) ;
+   console.log((marks.length) / 2) ;
 }
