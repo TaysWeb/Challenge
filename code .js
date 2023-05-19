1ad51273e8645db9ea680abb665807041c0aac38
TaysWeb <snelltararobin@gmail.com>
2023-05-19 12:36:06 +0200

    done

--- code .js
+++ code .js
@@ -1,31 +1,45 @@
+const marks = [39 , 42, 65, 71, 84] ;
+
 
 function calculateGrade() {
-    const marks = [39 , 42, 65, 71, 84, 93] ;
+    let grade = 'A' ;
 
-    // declaring my counter variable and additional variable 
-    let i = 0;
-    let total = marks.length([i]);
-
+    let average1 = calculateAverage() ;
+   
     // using the if statement by displaying the symbols 
 
-    if ( marks[0] <= 60 , i++ ) {           //value
-    console.log("Grade is F ")  ; 
+    if ( average1 > 90 && average1 < 100 ) {        //value
+         grade = 'A' ;
+    } 
+  if ( average1 > 80 &&  average1 < 89) {              //value
+         grade ='B'   ; 
     }
-    if ( marks[1] <= 70 , i++ ) {              //value
-    console.log("Grade is D ")  ; 
+  if (average1 > 69 &&  average1 < 80 ) {              //value
+        grade ='C'   ; 
+   }
+  if (average1 > 60 &&  average1 < 69 ) {              //value
+    grade ='D'   ; 
     }
-   if ( marks[2] <= 80 , i++ ) {               //value
-    console.log("Grade is  C ")  ; 
+  if ( average1 > 1 &&  average1 < 59 ) {              //value
+    grade ='F'   ; 
    }
-   if ( marks[3] <= 90 , i++ ) {              //value
-    console.log("Grade is B ")  ; 
-   }
-   if ( marks[4] <= 100, i++ ) {              //value
-    console.log("Grade is A ")  ; 
-}
+   console.log(average1);
+   console.log(grade);
 }
 
+
 function calculateAverage() {
-   let (i += total([i])) ;
-   console.log((marks.length) / 2) ;
+
+
+let average = 0;
+let total =0;
+let i =0;
+
+while (i < marks.length) {
+    total = total + marks[i] ;
+    i++ ;
+}
+average = total / 6;
+return average;
+
 }
